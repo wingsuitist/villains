b18767046f094fe5fc71d701c3cc9e7bafe1f181
Jonas Felix <jonas.felix@felixideas.ch>
2016-09-24 21:09:38 +0200

    implement master/detail view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,18 @@
 import { Component } from '@angular/core';
 import { Villain } from './shared';
 
+const VILLAINS: Villain[] = [
+  {id: 1, alias: 'Rebooter', power: 'Random Updates'},
+  {id: 2, alias: 'Break Changer', power: 'API crushing'},
+  {id: 3, alias: 'Not-Tester', power: 'Edit on Prod'},
+  {id: 4, alias: 'Super Spamer', power: 'Mail Fludding'},
+  {id: 5, alias: 'Mrs. DDOS', power: 'Service Overuse'},
+  {id: 6, alias: 'Trojan', power: 'Remote Control'},
+  {id: 7, alias: 'Randzombie', power: 'Encryptor'},
+  {id: 8, alias: 'Leacher', power: 'Net Overload'},
+  {id: 23, alias: 'Captain Spaghetticoder', power: 'Bug Creator'}
+];
+
 @Component({
   selector: 'vil-root',
   templateUrl: './app.component.html',
@@ -8,9 +20,10 @@ import { Villain } from './shared';
 })
 export class AppComponent {
   title = 'Villains unite!';
-  villain : Villain = {
-    id: 23,
-    alias: 'Captain Spaghetticoder',
-    power: 'Bug Creator'
-  };
+  villains = VILLAINS;
+  villain: Villain;
+
+  onSelect(villain: Villain): void {
+    this.villain = villain;
+  }
 }
